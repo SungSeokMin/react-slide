1bc0a282ecd4fba9965177a25d71b738b4ae6600
SungSeokMin <jkl154527@gmail.com>
2021-04-19 16:46:05 +0900

    💄 Image Slide

---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/house-1.jpg';
+import ImageTwo from '../images/house-2.jpg';
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi facilis aliquam exercitationem dolorum obcaecati facere pariatur molestiae corrupti illo aspernatur eos at ullam, quaerat excepturi suscipit repellendus sed in dolorem?',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim error, dolorum laboriosam similique minima quos.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: 'Modern Desings',
+  paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+  paragraphTwo:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim error, dolorum laboriosam similique minima quos.',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
